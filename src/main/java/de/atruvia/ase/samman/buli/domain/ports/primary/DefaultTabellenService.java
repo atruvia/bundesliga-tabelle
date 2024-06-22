7660a267912b279423c37c75e3d21b0e6ade4f65
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-06-22 13:05:04 +0200

    renamed

--- src/main/java/de/atruvia/ase/samman/buli/domain/ports/primary/DefaultTabellenService.java
+++ src/main/java/de/atruvia/ase/samman/buli/domain/ports/primary/DefaultTabellenService.java
@@ -6,10 +6,10 @@ import de.atruvia.ase.samman.buli.domain.Paarung;
 import de.atruvia.ase.samman.buli.domain.Tabelle;
 import de.atruvia.ase.samman.buli.domain.TabellenPlatz;
 import de.atruvia.ase.samman.buli.domain.ports.secondary.SpieltagRepo;
-import de.atruvia.ase.samman.buli.springframework.ServiceImplementation;
+import de.atruvia.ase.samman.buli.springframework.PrimaryPortImplementation;
 import lombok.RequiredArgsConstructor;
 
-@ServiceImplementation(TabellenService.class)
+@PrimaryPortImplementation(TabellenService.class)
 @RequiredArgsConstructor
 public class DefaultTabellenService implements TabellenService {
 
--- src/main/java/de/atruvia/ase/samman/buli/springframework/PrimaryPortImplementation.java
+++ src/main/java/de/atruvia/ase/samman/buli/springframework/PrimaryPortImplementation.java
@@ -9,16 +9,17 @@ import java.lang.annotation.Target;
 import org.springframework.stereotype.Service;
 
 /**
- * Prevents confusion when annotation the primary port with @Service coming from
- * jmolecules and the implementing class with @Service coming from
- * springframework.
+ * Prevents confusion when annotation the primary port with
+ * {@link org.jmolecules.ddd.annotation.Service} coming from jmolecules and the
+ * implementing class with {@link org.springframework.stereotype.Service} coming
+ * from springframework.
  * 
  * @author Peter Fichtner
  */
 @Service
 @Retention(RUNTIME)
 @Target(TYPE)
-public @interface ServiceImplementation {
+public @interface PrimaryPortImplementation {
 
 	/**
 	 * The implemented DDD-Service.
