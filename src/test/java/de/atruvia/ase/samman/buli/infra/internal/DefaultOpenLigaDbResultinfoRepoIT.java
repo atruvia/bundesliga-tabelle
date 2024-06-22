ff07dc9026d8f752145c734a58dc2bda69fb4cf0
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-06-22 11:52:56 +0200

    migrated to CartesianTest/introducing JUnit-Pioneer

--- src/test/java/de/atruvia/ase/samman/buli/infra/internal/DefaultOpenLigaDbResultinfoRepoIT.java
+++ src/test/java/de/atruvia/ase/samman/buli/infra/internal/DefaultOpenLigaDbResultinfoRepoIT.java
@@ -2,13 +2,9 @@ package de.atruvia.ase.samman.buli.infra.internal;
 
 import static de.atruvia.ase.samman.buli.infra.internal.OpenLigaDbResultinfoRepo.Resultinfo.endergebnisType;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-import java.util.List;
-
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junitpioneer.jupiter.cartesian.CartesianTest;
+import org.junitpioneer.jupiter.cartesian.CartesianTest.Values;
 import org.springframework.web.client.RestTemplate;
 
 class DefaultOpenLigaDbResultinfoRepoIT {
@@ -17,19 +13,12 @@ class DefaultOpenLigaDbResultinfoRepoIT {
 	OpenLigaDbResultinfoRepo sut = new DefaultOpenLigaDbResultinfoRepo(restTemplate,
 			new AvailableLeagueRepo(restTemplate));
 
-	@ParameterizedTest
-	@MethodSource("provideLeagueSeasonCombinations")
-	void endergebnisHasHighestGlobalId(String league, String season) {
+	@CartesianTest
+	void endergebnisHasHighestGlobalId( //
+			@Values(strings = { "bl1", "bl2" }) String league, //
+			@Values(strings = { "2022", "2023" }) String season //
+	) {
 		assertThat(endergebnisType(sut.getResultinfos(league, season)).name).isEqualTo("Endergebnis");
-	}
-
-	static List<Arguments> provideLeagueSeasonCombinations() {
-		return List.of( //
-				arguments("bl1", "2022"), //
-				arguments("bl1", "2023"), //
-				arguments("bl2", "2022"), //
-				arguments("bl2", "2023") //
-		);
 	}
 
 }
