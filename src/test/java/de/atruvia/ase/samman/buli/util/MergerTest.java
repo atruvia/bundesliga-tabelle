bde640edcd0752b25aeb12d7074c0c748a92eba8
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-06-23 18:42:06 +0200

    house keeping

--- src/test/java/de/atruvia/ase/samman/buli/util/MergerTest.java
+++ src/test/java/de/atruvia/ase/samman/buli/util/MergerTest.java
@@ -8,7 +8,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import java.util.Map;
-import java.util.function.BinaryOperator;
 
 import org.junit.jupiter.api.Test;
 
