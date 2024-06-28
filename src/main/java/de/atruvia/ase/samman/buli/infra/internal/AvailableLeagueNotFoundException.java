1a216f9ded9ec406392a0d5324d2555b05a2a330
Rusi Filipov <rusi.filipov@atruvia.de>
2024-06-28 18:17:48 +0200

    Assert AvailableLeagueNotFoundException properties in unit test

--- src/main/java/de/atruvia/ase/samman/buli/infra/internal/AvailableLeagueNotFoundException.java
+++ src/main/java/de/atruvia/ase/samman/buli/infra/internal/AvailableLeagueNotFoundException.java
@@ -1,12 +1,13 @@
 package de.atruvia.ase.samman.buli.infra.internal;
 
+import lombok.Getter;
+
+@Getter
 public class AvailableLeagueNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 4994065085648695879L;
 
-	// TODO verify if present in test
 	private final String league;
-	// TODO verify if present in test
 	private final String season;
 
 	public AvailableLeagueNotFoundException(String league, String season) {
