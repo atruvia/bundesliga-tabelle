c45bf628f198ef93b6f13a1f1a550cf614177507
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-07-05 17:37:09 +0200

    use #checkPropertiesOfFullSeason for all tests

--- src/test/java/de/atruvia/ase/samman/buli/infra/adapters/secondary/OpenLigaDbSpieltagRepoIT.java
+++ src/test/java/de/atruvia/ase/samman/buli/infra/adapters/secondary/OpenLigaDbSpieltagRepoIT.java
@@ -5,8 +5,11 @@ import static de.atruvia.ase.samman.buli.domain.Paarung.ErgebnisTyp.BEENDET;
 import static de.atruvia.ase.samman.buli.domain.TeamMother.teamBremen;
 import static de.atruvia.ase.samman.buli.domain.TeamMother.teamFrankfurt;
 import static de.atruvia.ase.samman.buli.domain.TeamMother.teamMuenchen;
+import static java.util.stream.Collectors.groupingBy;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.web.client.RestTemplate;
@@ -23,6 +26,7 @@ class OpenLigaDbSpieltagRepoIT {
 	@Test
 	void canRetrieveDataOf2022() {
 		var paarungen = repo().lade("bl1", "2022");
+		checkPropertiesOfFullSeason(paarungen);
 		var expected0 = Paarung.builder() //
 				.ergebnisTyp(BEENDET) //
 				.heim(entry(teamFrankfurt, 1)) //
@@ -34,6 +38,7 @@ class OpenLigaDbSpieltagRepoIT {
 	@Test
 	void canRetrieveDataOf2023() {
 		var paarungen = repo().lade("bl1", "2023");
+		checkPropertiesOfFullSeason(paarungen);
 		var expected0 = Paarung.builder() //
 				.ergebnisTyp(BEENDET) //
 				.heim(entry(teamBremen, 0)) //
@@ -45,9 +50,19 @@ class OpenLigaDbSpieltagRepoIT {
 	@Test
 	void canRetrieveDataOf2024() {
 		var paarungen = repo().lade("bl1", "2024");
-		assertThat(paarungen).hasSize(matchesOfFullSeasonOfTeams(18));
+		checkPropertiesOfFullSeason(paarungen);
+
 		assertThat(paarungen).element(94).satisfies(p -> matchIs(p, teamFrankfurt, teamBremen));
 		assertThat(paarungen).element(245).satisfies(p -> matchIs(p, teamBremen, teamFrankfurt));
+	}
+
+	void checkPropertiesOfFullSeason(List<Paarung> paarungen) {
+		assertThat(paarungen).hasSize(matchesOfFullSeasonOfTeams(18));
+		var byHeim = paarungen.stream().collect(groupingBy(p -> p.heim().team()));
+		var byGast = paarungen.stream().collect(groupingBy(p -> p.gast().team()));
+		assertThat(byHeim.keySet()).containsExactlyInAnyOrderElementsOf(byGast.keySet());
+		assertThat(byHeim).hasSize(18).allSatisfy((k, v) -> assertThat(v).hasSize(matchdays(18) / 2));
+		assertThat(byGast).hasSize(18).allSatisfy((k, v) -> assertThat(v).hasSize(matchdays(18) / 2));
 	}
 
 	void matchIs(Paarung paarung, Team expectedHeim, Team expectedGast) {
