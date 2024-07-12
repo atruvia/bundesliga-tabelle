a6eb1cc14a42a78cf92ec7996e5b51fe7055809c
Rusi Filipov <rusi.filipov@atruvia.de>
2024-07-12 10:58:10 +0200

    Use proper Spring bean configuration in TabellenHttpAdapterApprovalTest

--- src/test/java/de/atruvia/ase/samman/buli/infra/adapters/primary/TabellenHttpAdapterApprovalTest.java
+++ src/test/java/de/atruvia/ase/samman/buli/infra/adapters/primary/TabellenHttpAdapterApprovalTest.java
@@ -4,42 +4,31 @@ import static de.atruvia.ase.samman.buli.infra.adapters.secondary.OpenLigaDbSpie
 import static org.approvaltests.JsonApprovals.verifyJson;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 import de.atruvia.ase.samman.buli.domain.ports.primary.DefaultTabellenService;
 import de.atruvia.ase.samman.buli.domain.ports.primary.TabellenService;
 
-@SpringBootTest
+@WebMvcTest
 @AutoConfigureMockMvc
+@Import(TabellenHttpAdapterApprovalTest.OpenLigaConfig.class)
 class TabellenHttpAdapterApprovalTest {
 
 	@Autowired
-	TabellenHttpAdapter sut;
-
 	MockMvc mockMvc;
-
-	@BeforeEach
-	void setup() {
-		mockMvc = standaloneSetup(sut).build();
-	}
-
-	@MockBean
-	TabellenService tabellenService;
 
 	@Test
 	void approveWithRunningGames() throws Exception {
 		String league = "bl1";
 		String season = "2023-games-running";
-		sut = new TabellenHttpAdapter(new DefaultTabellenService(spieltagFsRepo()));
-		mockMvc = standaloneSetup(sut).build();
 		String jsonResponse = mockMvc.perform(get("/tabelle/" + league + "/" + season)) //
 				.andDo(print()) //
 				.andReturn() //
@@ -48,4 +37,11 @@ class TabellenHttpAdapterApprovalTest {
 		verifyJson(jsonResponse);
 	}
 
+	@TestConfiguration
+	static class OpenLigaConfig {
+		@Bean
+		TabellenService tabellenService() {
+			return new DefaultTabellenService(spieltagFsRepo());
+		}
+	}
 }
