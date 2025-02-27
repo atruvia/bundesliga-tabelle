20088df5c1ab57e7c2bde0d74ee49c97b8c3679e
Rusi Filipov <rusi.filipov@atruvia.de>
2025-02-27 16:22:31 +0100

    Improve OpenLigaDbSpieltagRepoIT

--- src/test/java/de/atruvia/ase/samman/buli/infra/adapters/secondary/OpenLigaDbSpieltagRepoIT.java
+++ src/test/java/de/atruvia/ase/samman/buli/infra/adapters/secondary/OpenLigaDbSpieltagRepoIT.java
@@ -26,49 +26,58 @@ import de.atruvia.ase.samman.buli.infra.internal.RestClient;
 
 class OpenLigaDbSpieltagRepoIT {
 
+	private final OpenLigaDbSpieltagRepo sut = repo();
+
+	private static OpenLigaDbSpieltagRepo repo() {
+		RestClient restClient = new RestClient(new RestTemplate());
+		return new OpenLigaDbSpieltagRepo(restClient,
+				new DefaultOpenLigaDbResultinfoRepo(restClient, new AvailableLeagueRepo(restClient)));
+	}
+
 	@Test
 	void canRetrieveDataOf2022() {
-		var paarungen = repo().lade("bl1", "2022");
-		checkPropertiesOfFullSeason(paarungen, 18);
-		var expected0 = Paarung.builder() //
+		var paarungen = sut.lade("bl1", "2022");
+		checkPropertiesOfFullSeason(paarungen);
+		var expected = Paarung.builder() //
 				.ergebnisTyp(BEENDET) //
 				.heim(entry(teamFrankfurt, 1)) //
 				.gast(entry(teamMuenchen, 6)) //
 				.build();
-		assertThat(paarungen).hasSize(matchesOfFullSeasonOfTeams(18)).element(0).isEqualTo(expected0);
+		assertThat(paarungen).hasSize(matchesOfFullSeasonOfTeams(18)).element(0).isEqualTo(expected);
 	}
 
 	@Test
 	void canRetrieveDataOf2023() {
-		var paarungen = repo().lade("bl1", "2023");
-		checkPropertiesOfFullSeason(paarungen, 18);
-		var expected0 = Paarung.builder() //
+		var paarungen = sut.lade("bl1", "2023");
+		checkPropertiesOfFullSeason(paarungen);
+		var expected = Paarung.builder() //
 				.ergebnisTyp(BEENDET) //
 				.heim(entry(teamBremen, 0)) //
 				.gast(entry(teamMuenchen, 4)) //
 				.build();
-		assertThat(paarungen).hasSize(matchesOfFullSeasonOfTeams(18)).element(0).isEqualTo(expected0);
+		assertThat(paarungen).hasSize(matchesOfFullSeasonOfTeams(18)).element(0).isEqualTo(expected);
 	}
 
 	@Test
 	void canRetrieveDataOf2024() {
-		var paarungen = repo().lade("bl1", "2024");
-		checkPropertiesOfFullSeason(paarungen, 18);
-		var expected0 = Paarung.builder() //
+		var paarungen = sut.lade("bl1", "2024");
+		checkPropertiesOfFullSeason(paarungen);
+		var expected = Paarung.builder() //
 				.ergebnisTyp(BEENDET) //
 				.heim(entry(teamDortmund, 2)) //
 				.gast(entry(teamFrankfurt, 0)) //
 				.build();
-		assertThat(paarungen).hasSize(matchesOfFullSeasonOfTeams(18)).element(6).isEqualTo(expected0);
+		assertThat(paarungen).hasSize(matchesOfFullSeasonOfTeams(18)).element(6).isEqualTo(expected);
 	}
 
-	void checkPropertiesOfFullSeason(List<Paarung> paarungen, int teams) {
-		assertThat(paarungen).hasSize(matchesOfFullSeasonOfTeams(teams));
+	void checkPropertiesOfFullSeason(List<Paarung> paarungen) {
+		int expectedTeams = 18;
+		assertThat(paarungen).hasSize(matchesOfFullSeasonOfTeams(expectedTeams));
 		var byHeim = teams(paarungen, p -> p.heim().team());
 		var byGast = teams(paarungen, p -> p.gast().team());
 		assertThat(byHeim.keySet()).containsExactlyInAnyOrderElementsOf(byGast.keySet());
-		assertThat(byHeim).hasSize(teams).allSatisfy((k, v) -> assertThat(v).hasSize(matchdaysPerRound(teams)));
-		assertThat(byGast).hasSize(teams).allSatisfy((k, v) -> assertThat(v).hasSize(matchdaysPerRound(teams)));
+		assertThat(byHeim).hasSize(expectedTeams).allSatisfy((k, v) -> assertThat(v).hasSize(matchdaysPerRound(expectedTeams)));
+		assertThat(byGast).hasSize(expectedTeams).allSatisfy((k, v) -> assertThat(v).hasSize(matchdaysPerRound(expectedTeams)));
 	}
 
 	void matchIs(Paarung paarung, Team expectedHeim, Team expectedGast) {
@@ -84,12 +93,6 @@ class OpenLigaDbSpieltagRepoIT {
 
 	void teamIs(Entry entry, Team expectedTeam) {
 		assertThat(entry.team()).isEqualTo(expectedTeam);
-	}
-
-	OpenLigaDbSpieltagRepo repo() {
-		RestClient restClient = new RestClient(new RestTemplate());
-		return new OpenLigaDbSpieltagRepo(restClient,
-				new DefaultOpenLigaDbResultinfoRepo(restClient, new AvailableLeagueRepo(restClient)));
 	}
 
 	int matchesOfFullSeasonOfTeams(int teams) {
